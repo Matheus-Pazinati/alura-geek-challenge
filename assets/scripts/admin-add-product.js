e9494925cbedb86906ce333b6c89a1b2f9d46c88
Matheus Pazinati <matheuspazinati@gmail.com>
2022-06-17 04:40:14 -0300

    Permite insercao da imagem do produto atraves de drag-and-drop

--- assets/scripts/admin-add-product.js
+++ assets/scripts/admin-add-product.js
@@ -18,4 +18,20 @@ function loadImageFromInput() {
     })
 }
 
+function dragAndDropImages() {
+  let imageContainer = document.getElementById('image-container')
+  imageContainer.addEventListener('dragover', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      event.dataTransfer.dropEffect = 'copy';
+  })
+  imageContainer.addEventListener('drop', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      const fileList = event.dataTransfer.files;
+      readImage(fileList[0])
+  })
+}
+
 loadImageFromInput()
+dragAndDropImages()
