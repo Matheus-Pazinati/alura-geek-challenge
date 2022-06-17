ddaf5a0273e32a9f9013b20ff526d0e94f807380
Matheus Pazinati <matheuspazinati@gmail.com>
2022-06-17 04:37:33 -0300

    Adiciona funcao para pre-visualizacao da imagem do produto a ser adicionado

--- assets/scripts/admin-add-product.js
+++ assets/scripts/admin-add-product.js
@@ -1,3 +1,21 @@
 import { openCloseSearchBox } from "./open-close-searchbox.js";
-
 openCloseSearchBox()
+
+let uploadedImageURL;
+
+function readImage(file) {
+  uploadedImageURL = URL.createObjectURL(file);
+  let imageContainer = document.getElementById('image-container')
+  imageContainer.innerHTML = ''
+  imageContainer.style.background = `url(${uploadedImageURL}) no-repeat center/cover`
+}
+
+function loadImageFromInput() {
+    let inputFile = document.querySelector('input[type=file]')
+    inputFile.addEventListener('change', () => {
+        let image = inputFile.files[0]
+        readImage(image)
+    })
+}
+
+loadImageFromInput()
