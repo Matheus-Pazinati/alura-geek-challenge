53b21da19a6b2bfce5ec5b99e9de41faeebc8b27
Matheus Pazinati <matheuspazinati@gmail.com>
2022-06-19 03:29:29 -0300

    Separando cada funcao para pre-visualizar a imagem do produto que sera adicionado em modulos

---
+++ assets/scripts/add-products-components/add-products-main.js
@@ -0,0 +1,7 @@
+import { openCloseSearchBox } from "../open-close-searchbox.js";
+import { loadImageFromInput } from './load-image-input.js';
+import { dragAndDropImages } from './load-image-dragndrop.js'
+
+openCloseSearchBox()
+loadImageFromInput()
+dragAndDropImages()
