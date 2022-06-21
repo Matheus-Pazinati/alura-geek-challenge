564b92677b1578901218598bfb7154becce19476
Matheus Pazinati <matheuspazinati@gmail.com>
2022-06-21 20:39:15 -0300

    Cria funcao que sera responsavel por enviar os dados para o datocms

--- assets/scripts/add-products-components/add-products-main.js
+++ assets/scripts/add-products-components/add-products-main.js
@@ -5,3 +5,45 @@ import { dragAndDropImages } from './load-image-dragndrop.js'
 openCloseSearchBox()
 loadImageFromInput()
 dragAndDropImages()
+
+function addNewProduct() {
+  const productName = document.querySelector('[data-input-name]')
+  const productCategory = document.querySelector('[data-input-category]')
+  const productPrice = document.querySelector('[data-input-price]')
+  const productDescription = document.querySelector('[data-input-description]')
+
+  fetch('https://site-api.datocms.com/items', {
+    method: 'POST',
+    headers: {
+      'X-Api-Version': '3',
+      'Authorization': 'Bearer e16fc5dad5efc05d18c48a26a709ac',
+      'Accept': 'application/json',
+      'Content-Type': 'application/vnd.api+json'
+    },
+    body: JSON.stringify({
+      data: {
+        type: 'item',
+        attributes: {
+          name: `${productName.value}`,
+          category: `${productCategory.value}`,
+          price: `${productPrice.value}`,
+          description: `${productDescription.value}`
+        },
+        relationships: {
+          item_type: {
+            data: {
+              type: 'item_type',
+              id: '489314'
+            }
+          }
+        }
+      }
+    })
+  })
+}
+
+let form = document.querySelector('[data-form]')
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  addNewProduct()
+})
