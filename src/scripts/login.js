77084b19ca841887034d69c2e3b453b425fbfed5
Matheus Pazinati <matheuspazinati@gmail.com>
2023-11-07 11:55:57 -0300

    create the authentication flow

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,9 +1,42 @@
+import { FormValidator } from "./form-validator.js";
 import { openCloseSearchBox } from "./open-close-searchbox.js";
 
 openCloseSearchBox()
 
 const loginButton = document.querySelector('[data-button-submit]')
 loginButton.addEventListener('click', (event) => {
-  event.preventDefault()
-  window.location.href = '../pages/admin-home.html'
+  FormValidator.handleSubmit(event, authenticateUser)
 })
+
+async function authenticateUser(inputs) {
+  const formValues = inputs.reduce((acc, current) => {
+    acc[current.id] = current.value
+    return acc
+  }, {
+    email: '',
+    password: '',
+  })
+
+  const response = await fetch("http://localhost:3000/authentication", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: 'include',
+    body: JSON.stringify(formValues)
+  })
+
+  const jsonResponse = await response.json()
+
+  if (!response.ok) {
+    if (response.status == 401) {
+      FormValidator.showInputError(
+        document.getElementById(jsonResponse.path),
+        jsonResponse.message
+      )
+      throw new Error(jsonResponse.message)
+    }
+  } else {
+    window.location.href="admin-home.html"
+  }
+}
